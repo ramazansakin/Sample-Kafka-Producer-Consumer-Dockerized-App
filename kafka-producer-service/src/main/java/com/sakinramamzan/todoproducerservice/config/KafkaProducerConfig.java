29b7be5871a506b9d9e5a95f11e65a15f2e910c1
Ramazan <ramazansakin63@gmail.com>
2020-07-29 15:59:31 +0300

    producer service impl

---
+++ kafka-producer-service/src/main/java/com/sakinramamzan/todoproducerservice/config/KafkaProducerConfig.java
@@ -0,0 +1,53 @@
+package com.sakinramamzan.todoproducerservice.config;
+
+import com.sakinramamzan.todoproducerservice.model.Todo;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    // Kafka String message consumer bean
+    @Bean
+    public ProducerFactory<String, String> StringProducerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaStringTemplate() {
+        return new KafkaTemplate<>(StringProducerFactory());
+    }
+
+    // Kafka To-do object consumer bean
+    @Bean
+    public ProducerFactory<String, Object> TodoProducerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTodoTemplate() {
+        return new KafkaTemplate<>(TodoProducerFactory());
+    }
+
+}
---
+++ kafka-producer-service/src/main/java/com/sakinramamzan/todoproducerservice/resource/KafkaResource.java
@@ -0,0 +1,59 @@
+package com.sakinramamzan.todoproducerservice.resource;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sakinramamzan.todoproducerservice.model.Todo;
+import com.sakinramamzan.todoproducerservice.model.UserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/consumer")
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaResource {
+
+    private final KafkaTemplate<String, String> kafkaStringTemplate;
+
+    private final KafkaTemplate<String, Object> kafkaTodoTemplate;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+    }
+
+    private static final String TOPIC_STRING = "Kafka_Sample_Topic";
+    private static final String TOPIC_TODO = "Kafka_Todo_Topic";
+    private static final String TOPIC_USER = "Kafka_User_Topic";
+
+    @GetMapping("/publish/{message}")
+    public String publishMessage(@PathVariable final String message) {
+        log.info("String Message : " + message);
+        kafkaStringTemplate.send(TOPIC_STRING, message);
+
+        return "Message published successfully : " + message;
+    }
+
+    @PostMapping("/publish-todo")
+    public String publishTodo(@RequestBody Todo todo) {
+        log.info("Todo Message : " + todo);
+        kafkaTodoTemplate.send(TOPIC_TODO, todo);
+
+        return "Todo published successfully : " + todo;
+    }
+
+    @PostMapping("/publish-user")
+    public String publishUserInfo(@RequestBody UserInfo userInfo) {
+        log.info("UserInfo Message : " + userInfo);
+        kafkaTodoTemplate.send(TOPIC_USER, userInfo);
+
+        return "UserInfo published successfully : " + userInfo;
+    }
+
+}
